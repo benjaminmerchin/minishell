f93ff9080bf8160c528314399ac10e2369f3d92f
benjaminmerchin <benjaminmerchin@gmail.com>
2021-04-12 13:43:20 +0200

    test

--- minishell.c
+++ minishell.c
@@ -6,3 +6,38 @@ int		main(int argc, char **argv)
 
 	return (5);
 }
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmerchin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/25 02:46:33 by bmerchin          #+#    #+#             */
+/*   Updated: 2020/11/18 16:32:57 by bmerchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	len;
+	char			*str;
+
+	if (!s)
+		return (ft_calloc(1, 1));
+	i = 0;
+	len = ft_strlen(s);
+	if (!(str = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = f(i, (char)s[i]);
+		i++;
+	}
+	str[len] = '\0';
+	return (str);
+}
