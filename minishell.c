e185dace731b70f01753c3c96030232882ed178c
benjaminmerchin <benjaminmerchin@gmail.com>
2021-04-12 13:44:17 +0200

    test

--- minishell.c
+++ minishell.c
@@ -3,8 +3,8 @@
 int		main(int argc, char **argv)
 {
 
-
-	return (5);
+	printf("hello c'est benjamin");
+	return (5959595);
 }
 
 /* ************************************************************************** */
