256272ad302ab25ba427ead1e26133bcfbdc2d6a
benjaminmerchin <benjaminmerchin@gmail.com>
2021-05-24 13:38:05 +0200

    norme in progress

---
+++ execution/add_env_or_not_found.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_env_or_not_found.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 19:00:55 by user42            #+#    #+#             */
+/*   Updated: 2021/03/18 19:04:02 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	command_not_found(t_a *a, int *i)
+{
+	a->dollar_question = 127;
+	ft_putstr("\033[031m");
+	ft_putstr(MINISHELL_NAME);
+	ft_putstr(": ");
+	ft_putstr_fd(a->raw[*i].str, 1);
+	ft_putstr_fd(": command not found\n", 1);
+	while (a->raw[*i].str != 0 && a->raw[*i].type != '|' \
+	&& a->raw[*i].type != ';')
+		(*i)++;
+	ft_putstr_fd("\033[0m", 1);
+}
+
+void	add_env(t_a *a, int *i)
+{
+	int		ret;
+	t_list	*lst;
+
+	a->dollar_question = 0;
+	while (a->raw[*i].str != 0 && a->raw[*i].type != '|' \
+	&& a->raw[*i].type != ';')
+	{
+		ret = ft_verification_content(a->raw[*i].str, a, i);
+		if (ret > 0)
+		{
+			lst = a->lst_env;
+			while (lst)
+			{
+				if  (ft_strncmp(a->raw[*i].str, lst->content, ret) == 0)
+				{
+					free(lst->content);
+					lst->content = ft_strdup(a->raw[*i].str);
+				}
+				lst = lst->next;
+			}
+			(*i)++;
+		}
+		else
+			command_not_found(a, i);
+	}
+}
+
+void	add_env_or_command_not_found(t_a *a, int *i)
+{
+	int	j;
+
+	j = 0;
+	while (a->raw[*i].str[j] != '\0' && a->raw[*i].str[j] != '=')
+		j++;
+	if (a->raw[*i].str[j] == '=' && j != 0)
+		add_env(a, i);
+	else
+		command_not_found(a, i);
+}
--- execution/fork_wait_execute.c
+++ execution/fork_wait_execute.c
@@ -12,24 +12,21 @@
 
 #include "../includes/minishell.h"
 
-/*
-(void)a;
-(void)i;
-*/
-
 char	**put_args_into_an_array(t_a *a, int *i)
 {
 	char	**table;
-	int 	j;
+	int		j;
 
 	j = 0;
-	while (a->raw[*i + j].str != 0 && a->raw[*i + j].type != '|' && a->raw[*i + j].type != ';')
+	while (a->raw[*i + j].str != 0 && a->raw[*i + j].type != '|'
+		&& a->raw[*i + j].type != ';')
 		j++;
 	table = malloc(sizeof(char *) * (j + 1));
 	if (! table)
 		ft_exit_clean(a, "Error\nMalloc Failed\n");
 	j = 0;
-	while (a->raw[*i + j].str != 0 && a->raw[*i + j].type != '|' && a->raw[*i + j].type != ';')
+	while (a->raw[*i + j].str != 0 && a->raw[*i + j].type != '|'
+		&& a->raw[*i + j].type != ';')
 	{
 		table[j] = a->raw[*i + j].str;
 		j++;
@@ -43,8 +40,8 @@ char	**put_aenv_into_an_array(t_a *a, int *i)
 	char	**table;
 	t_list	*lst;
 	int		j;
-	(void)i;
 
+	(void)i;
 	j = ft_lstsize(a->lst_env);
 	table = malloc(sizeof(char *) * (j + 1));
 	if (! table)
@@ -63,12 +60,12 @@ char	**put_aenv_into_an_array(t_a *a, int *i)
 
 char	*return_str_from_env(t_a *a, char *str)
 {
-	t_list *lst;
+	t_list	*lst;
 
 	lst = a->lst_env;
 	while (lst)
 	{
-		if  (ft_strncmp(str, lst->content, ft_strlen(str)) == 0)
+		if (ft_strncmp(str, lst->content, ft_strlen(str)) == 0)
 		{
 			return (lst->content + ft_strlen(str));
 		}
@@ -79,7 +76,7 @@ char	*return_str_from_env(t_a *a, char *str)
 
 void	ft_free_table(char **table)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (table[i])
@@ -93,8 +90,8 @@ void	ft_free_table(char **table)
 
 char	*triple_strjoin(char *s1, char *s2, char *s3)
 {
-	char *temp;
-	char *ret;
+	char	*temp;
+	char	*ret;
 
 	temp = ft_strjoin_libft(s1, s2);
 	ret = ft_strjoin_libft(temp, s3);
@@ -103,18 +100,15 @@ char	*triple_strjoin(char *s1, char *s2, char *s3)
 	return (ret);
 }
 
-char	*does_this_function_exist(t_a *a, int *i) //we want to dup and return the correct path.
-{	//test on the current repo and on the path repo
-	(void)i;
-	char	**temp;
-	char	*path;
+char	*does_this_function_exist(t_a *a, int *i, int j)
+{
+	char		**temp;
+	char		*path;
 	struct stat	buffer;
-	int		j;
-	char	*str;
+	char		*str;
 
-	if (stat(a->raw[*i].str, &buffer) == 0)//first we try to execute the command locally
+	if (stat(a->raw[*i].str, &buffer) == 0)
 		return (ft_strdup(a->raw[*i].str));
-	j = 0;
 	path = return_str_from_env(a, "PATH=");
 	temp = ft_split(path, ':');
 	while (temp[j])
@@ -142,20 +136,20 @@ void	fork_wait_execute(t_a *a, int *i)
 	char	**aenv;
 	char	*path;
 
-	path = does_this_function_exist(a, i);
+	path = does_this_function_exist(a, i, 0);
 	if (path == NULL)
 		add_env_or_command_not_found(a, i);
 	argv = put_args_into_an_array(a, i);
 	aenv = put_aenv_into_an_array(a, i);
 	pid = fork();
-	if (pid == 0) //we are in the child if pid = 0
+	if (pid == 0)
 	{
-		if (execve(path, argv, aenv) == -1) //a->raw[*i].str //exiter le chemin de av[0]
-			exit (a->dollar_question); //remplace with strerrer & errno //on kill syst // on doit exit clean ?
+		if (execve(path, argv, aenv) == -1)
+			exit (a->dollar_question);
 	}
-	else if (pid < 0) //in case of fail
+	else if (pid < 0)
 		ft_exit_clean(a, "Error:\nFork failed\n");
-	else //parent, if the pid value is not 0
+	else
 	{
 		waitpid(pid, &status, WUNTRACED);
 		while (!WIFEXITED(status) && !WIFSIGNALED(status))
@@ -164,49 +158,11 @@ void	fork_wait_execute(t_a *a, int *i)
 		if (g_fantaisie >= 127)
 			a->dollar_question = g_fantaisie;
 	}
-	while (a->raw[*i].str != 0 && a->raw[*i].type != '|' && a->raw[*i].type != ';')
+	while (a->raw[*i].str != 0 && a->raw[*i].type
+		!= '|' && a->raw[*i].type != ';')
 		(*i)++;
 	free (argv);
 	free (aenv);
 	free (path);
 	return ;
 }
-
-/*
-void	fork_wait_execute_backup(t_a *a, int *i)
-{
-	pid_t	pid;
-	int		status;
-	char	**argv;
-	char	**aenv;
-	char	*path;
-
-	path = does_this_function_exist(a, i);
-	if (path == NULL)
-		add_env_or_command_not_found(a, i);
-	argv = put_args_into_an_array(a, i);
-	aenv = put_aenv_into_an_array(a, i);
-	pid = fork();
-	if (pid == 0) //we are in the child if pid = 0
-	{
-		if (execve(path, argv, aenv) == -1) //a->raw[*i].str //exiter le chemin de av[0]
-			exit (0); //remplace with strerrer & errno //on kill syst // on doit exit clean ?
-	}
-	else if (pid < 0) //in case of fail
-		ft_exit_clean(a, "Error:\nFork failed\n");
-	else //parent, if the pid value is not 0
-	{
-		//write(1, "##########", 10);
-		waitpid(pid, &status, WUNTRACED);
-		while (!WIFEXITED(status) && !WIFSIGNALED(status))
-			waitpid(pid, &status, WUNTRACED);
-		a->dollar_question = (status % 255) % 127;
-	}
-	while (a->raw[*i].str != 0 && a->raw[*i].type != '|' && a->raw[*i].type != ';')
-		(*i)++;
-	free (argv);
-	free (aenv);
-	free (path);
-	return ;
-}
-*/
